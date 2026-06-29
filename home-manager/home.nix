2ceb1ada7702f17c5c589b5a5289e09e1c16e837
ruis2615 <ruis@ruis.app>
2026-06-29 19:27:22 +0900

    feat: enhance Home Manager configuration by adding Git settings and updating package list

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,12 +6,25 @@
   home.stateVersion = "26.05";  # 変更しない
 
   home.packages = [
+  ];
 
+  home.packages = with pkgs; [
+    git
   ];
 
   home.file = {
-
+    ".gitconfig".source = ./git/.gitconfig;
   };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "ruis2615";
+        email = "ruis@ruis.app";
+      }
+    }
+  }
 
   home.sessionVariables = {
 
