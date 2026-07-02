ff729084d24ed09321d0a93aa25d69ec2dcc89be
ruis2615 <ruis@ruis.app>
2026-07-02 10:59:05 +0900

    feat: 初期ファイルらを追加

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Home Manager configuration of ruis2615";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."ruis2615" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home-manager/home.nix ];
+      };
+    };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+
+  home.username = "ruis2615";
+  home.homeDirectory = "/Users/ruis2615";
+  home.stateVersion = "26.05";
+
+  home.packages = [
+
+  ];
+
+  home.file = {
+
+  };
+
+  home.sessionVariables = {
+
+  };
+
+  programs.home-manager.enable = true;
+}
